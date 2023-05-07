c59ab0238662ebc583e2a4b04a5549db73309748
Pedro Hammes <hammespedro4@gmail.com>
2023-05-07 08:31:36 -0300

    reset do projeto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { playerRegistration, players } from "./Modules/player-registration.js"
 const board = document.getElementById('board')
 const registerBtn = document.getElementById('register-btn')
 const playBtn = document.getElementById('play-btn')
+let players = []
 let round
 let playerTurn = document.getElementById('player-turn')
 let boardSquares = [0, 1, 2, 3, 4, 5, 6, 7, 8]
@@ -19,7 +19,36 @@ let vp2 = document.getElementById('VP2')
 let dp2 = document.getElementById('DP2')
 let ep2 = document.getElementById('EP2')
 
-registerBtn.addEventListener('click', playerRegistration)
+//REGISTRO DE JOGADORES
+registerBtn.addEventListener('click', function () {
+  const player1 = document.getElementById('player-1').value
+  const player2 = document.getElementById('player-2').value
+
+  if(player1 === player2 || player1 === ''){
+    document.getElementById('player-1').value = ''
+    document.getElementById('player-2').value = ''
+    document.getElementById('player-1').style.border = `2px solid ${p1Color}`
+    document.getElementById('player-1').setAttribute('placeholder', '*Obrigatório')
+    document.getElementById('player-2').style.border = `2px solid ${p1Color}`
+    document.getElementById('player-2').setAttribute('placeholder', '*Obrigatório')
+
+    alert(`[!ERROR]\nPlayer 1 e PLayer 2 precisam ser definidos e não podem ter o mesmo nome.\n\nPlayer 1: ${player1}\nPlayer 2: ${player2}`)
+
+  }else{
+    players.push([player1])
+    players[0].push(0)
+    players[0].push(0)
+    players[0].push(0)
+    players.push([player2])
+    players[1].push(0)
+    players[1].push(0)
+    players[1].push(0)
+  
+    document.getElementById('p1').innerText = player1
+    document.getElementById('p2').innerText = player2
+  }
+  return console.log(players)
+})
 
 //INÍCIO DE JOGO
   playBtn.addEventListener('click', function () {
